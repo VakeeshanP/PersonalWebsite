93c91e000af8924423631db8ebc9d71cea356ee3
Vakeeshan Paskaran <99076427+VakeeshanP@users.noreply.github.com>
2023-11-01 20:10:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelector('.Menu');
-    const menuList = document.querySelector('.List ul');
-    menu.addEventListener('click', () => {
-        menuList.classList.toggle('ShowMenu');
-    });
-});
